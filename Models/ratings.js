6d0f248db58a2885c86da52da08f61b16aa6a27a
RavindraMedhat <ravindrasinhmedhat@gmail.com>
2023-12-13 10:19:16 +0530

    add login system

--- Models/ratings.js
+++ Models/ratings.js
@@ -1,17 +1,18 @@
 // Create a ratings.model.js file
 const mongoose = require('mongoose');
 
-const ratingSchema = new mongoose.Schema({
-    CompanyName : String,
-    Data :[{
-        Area : String,
-        PerformanceIndicators :[{
-            Performance_Indicator : String,
-            rating : Number
+    const ratingSchema = new mongoose.Schema({
+        CompanyName : String,
+        Data :[{
+            Area : String,
+            PerformanceIndicators :[{
+                Performance_Indicator : String,
+                rating : Number
+            }]
         }]
-
-    }]
-});
+    },{
+        timestamps: true
+    });
 
 const Rating = mongoose.model('Rating', ratingSchema);
 
---
+++ Routers/UserRout.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const app = express()
+const User = require("../models/Users");
+
+app.get('/register', (req, res) => {
+    res.render('register');
+});
+
+app.post('/register', async (req, res) => {
+    try {
+        const newUser = new User({
+            Email: req.body.email,
+            Password: req.body.password,
+            CompanyName: req.body.companyName,
+            ContactPerson: req.body.contactPerson,
+            MobileNumber: req.body.mobileNumber,
+        });
+        newUser.Roal = "Customer";
+        // Save the user to the database
+        const savedUser = await newUser.save();
+
+        res.redirect("./login");
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+
+app.get("/login", (req, res) => {
+    res.render("login", { errorMessage: "" });
+})
+
+app.post("/login", (req, res) => {
+    const { Email, Password } = req.body;
+
+    User.findOne({ Email })
+        .then((data) => {
+            if (!data || data.Password !== Password) {
+                return res.render("login", { errorMessage: "Invalid email or password" });
+            } else {
+                req.session.data = data;
+                res.redirect("/Company/companyList");
+            }
+        })
+        .catch((error) => {
+            console.error("Error during login:", error);
+            res.status(500).send("Internal Server Error");
+        });
+});
+
+app.get("/logout", (req, res) => {
+    req.session.data = null;
+    res.redirect("/user/login")
+});
+
+module.exports = app;
